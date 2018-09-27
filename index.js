d4c2e47842a0336bf619bb4977d78427e1fd2baf
Johnathon Wood <jaywood128@gmail.com>
2018-09-27 20:37:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function getFirstSelector(selector) {
+  document.querySelector(selector)
+   return document.querySelector(selector)
+}
+function nestedTarget() {
+   document.querySelector('#nested .target')
 
+   return document.querySelector('#nested .target')
+}
