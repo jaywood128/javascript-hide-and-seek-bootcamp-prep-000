9896c414652b7168de15656806d7a5cc2bf4efe5
Johnathon Wood <jaywood128@gmail.com>
2018-09-28 13:15:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ function nestedTarget() {
 }
 function increaseRankBy(n) {
   const list = document.querySelector('ul.ranked-list li')
-   for(i = 0; i <list.length; i++ ){
+   for(let i = 0; i <list.length; i++ ){
        list[i].innerHTML = parseInt((i + n).toString)
    }
    return list
 }
 function deepestChild() {
-
+  document.querySelector()
+  
 }
