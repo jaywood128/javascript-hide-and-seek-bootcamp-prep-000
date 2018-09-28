0573bb7f8214b328b640698fa41af80ab373fce0
Johnathon Wood <jaywood128@gmail.com>
2018-09-28 12:21:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function getFirstSelector(selector) {
   document.querySelector(selector)
+<<<<<<< HEAD
    return document.querySelector(selector)
 }
 function nestedTarget() {
    document.querySelector('#nested .target')
 
    return document.querySelector('#nested .target')
+=======
+   return selector
+}
+function nestedTarget(element) {
+  var element = document.getElementsById('#nested').target
+>>>>>>> 2a5768cb96bedfa1a53efeba977ea4cffdcb44c0
 }
